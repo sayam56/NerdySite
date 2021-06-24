71d6e9f09ece38606dcc4eb5ebc2aaf10c31e070
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-06-24 23:14:22 +0600

    projectViewMore

--- js/view_more.js
+++ js/view_more.js
@@ -1,24 +1,31 @@
-// $(function(){
-//     $(".image-hidden").slice(0,3).show();
-//     $("#viewMore").on('click',function(e){
-//         e.preventDefault();
-//         $(".image-hidden:hidden").slice(0,3).slideDown();
-//         if($("image-hidden:hidden").length==0){
-//             $()
-//         }
-//     });
-// });
 
 $(function () {
-    $(".moreBox").slice(0, 3).show();
-      if ($(".blogBox:hidden").length != 0) {
-        $("#loadMore").show();
-      }   
-      $("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        $(".moreBox:hidden").slice(0, 6).slideDown();
-        if ($(".moreBox:hidden").length == 0) {
-          $("#loadMore").fadeOut('slow');
-        }
-      });
-    });
+  $("#loadMore").on('click', function (e) {
+    e.preventDefault();
+
+    let classList = ["gallery-block","mix", "all" , "col-lg-4", "col-md-6" , "col-sm-12"];
+
+
+    for(var i=1 ; i<=7 ; i++){
+      var element;
+      element = document.getElementById("hiddenBox"+i);
+      element.classList.add(...classList);
+      $(element).fadeIn("slow");
+    }
+
+    // var element1 = document.getElementById("hiddenBox1");
+    // var element2 = document.getElementById("hiddenBox2");
+    // var element3 = document.getElementById("hiddenBox3");
+
+
+    // console.log(element1);
+    // element1.classList.add(...classList);
+    // element1.style.display = "inline-block";
+
+    // element2.classList.add(...classList);
+    // element2.style.display = "inline-block";
+
+    // element3.classList.add(...classList);
+    // element3.style.display = "inline-block";
+  });
+});
