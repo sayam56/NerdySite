2a343f241acef3bb7a0e8f7909be4a454043ca12
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-06-25 01:02:00 +0600

    view Less functionality Implemenetd

--- js/view_more.js
+++ js/view_more.js
@@ -3,16 +3,42 @@ $(function () {
   $("#loadMore").on('click', function (e) {
     e.preventDefault();
 
+    var loadbtnName = document.getElementById("loadBtnID");
+  
+
     let classList = ["gallery-block","mix", "all" , "col-lg-4", "col-md-6" , "col-sm-12"];
 
+    if(loadbtnName.innerHTML.localeCompare("View Less") == 0){
 
-    for(var i=1 ; i<=7 ; i++){
-      var element;
-      element = document.getElementById("hiddenBox"+i);
-      element.classList.add(...classList);
-      $(element).fadeIn("slow");
+      for(var i=1 ; i<=7 ; i++){
+        var element;
+        element = document.getElementById("hiddenBox"+i);
+        $(element).fadeOut("slow");
+        // element.classList.remove(...classList);
+      }
+
+      loadbtnName.innerHTML = "View More";
+    }
+    else if(loadbtnName.innerHTML.localeCompare("View More") == 0){
+
+      for(var i=1 ; i<=7 ; i++){
+        var element;
+        element = document.getElementById("hiddenBox"+i);
+        element.classList.add(...classList);
+        $(element).fadeIn("slow");
+      }
+
+      loadbtnName.innerHTML = "View Less";
+
     }
 
+    
+   
+
+
+
+
+    // document.getElementById("loadBtnID").innerHTML = 'View Less';
     // var element1 = document.getElementById("hiddenBox1");
     // var element2 = document.getElementById("hiddenBox2");
     // var element3 = document.getElementById("hiddenBox3");
