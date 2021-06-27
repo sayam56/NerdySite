c94dffddfac2d07b283c6b8093c4fe8672a29b2b
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-06-27 17:46:09 +0600

    faq email

--- sendQuestion.php
+++ sendQuestion.php
@@ -8,15 +8,13 @@ define( "RECIPIENT_EMAIL", "admin@nerdevolution.tech" );
 $success = false;
 $userName = isset( $_POST['username'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['username'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$senderPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$userSubject = isset( $_POST['subject'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
-if ( $userName && $senderEmail && $senderPhone && $userSubject && $message) {
+if ( $userName && $senderEmail && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
   $headers = "From: " . $userName . "";
-  $msgBody = " Name: ". $userName .  "\n Email: ". $senderEmail . "\n Phone: ". $senderPhone . "\n Subject: ". $userSubject . "\n Message: " . $message . "";
+  $msgBody = " Name: ". $userName .  "\n Email: ". $senderEmail . "\n Subject: FAQ \n Message: " . $message . "";
   $success = mail( $recipient, $headers, $msgBody );
 
   //Set Location After Successful Submission
